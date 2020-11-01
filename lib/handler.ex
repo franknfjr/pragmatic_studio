06a31675a8fd381d0eb63cf5af38c7c2771686da
franknfjr <frankdonascimentojr@gmail.com>
2020-11-01 02:51:24 -0300

    reading an external file from app

--- lib/handler.ex
+++ lib/handler.ex
@@ -46,6 +46,23 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    file = 
+    Path.expand("../../pages/", __DIR__)
+    |> Path.join("abount.html")
+    
+    case File.read(file) do
+      {:ok, content} ->
+        %{conv | status: 200, resp_body: content}
+
+      {:error, :enoent} ->
+        %{conv | status: 404, resp_body: "File not found."}
+
+      {:error, reason} ->
+        %{conv | status: 500, resp_body: "File Error: #{reason}."}
+    end
+  end
+
   def route(%{method: "GET", path: "/doggos/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Doggos #{id}"}
   end
@@ -118,6 +135,18 @@ IO.puts(response)
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
