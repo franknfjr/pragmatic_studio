50f22140b9b93a5b174b4d0e67519d1c3abff290
franknfjr <frankdonascimentojr@gmail.com>
2020-11-01 00:52:34 -0300

    refactoring

--- lib/handler.ex
+++ lib/handler.ex
@@ -10,12 +10,12 @@ defmodule Servy.Handler do
   end
 
   def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} not exist!"
+    IO.puts("Warning: #{path} not exist!")
     conv
   end
 
   def track(conv), do: conv
-  
+
   def rewrite_path(%{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
@@ -34,23 +34,23 @@ defmodule Servy.Handler do
     %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(conv, "GET", "/doggos/" <> id) do
+  def route(%{method: "GET", path: "/doggos/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Doggos #{id}"}
   end
 
-  def route(conv, _method, path) do
+  def route(%{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
